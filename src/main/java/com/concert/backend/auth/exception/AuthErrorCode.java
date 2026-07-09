fc5cd7bd769d4cfd7b40d75227ccf0eefe1b60e2
LimdaeIl <optional94@naver.com>
2026-07-09 15:35:56 +0900

    #9: 커스텀 예외 구현

---
+++ src/main/java/com/concert/backend/auth/exception/AuthErrorCode.java
@@ -0,0 +1,26 @@
+package com.concert.backend.auth.exception;
+
+import com.concert.backend.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements ErrorCode {
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "The email '%s' is already registered."),
+    DUPLICATE_PHONE(HttpStatus.CONFLICT, "The phone number '%s' is already in use.");
+
+    private final HttpStatus httpStatus;
+    private final String messageTemplate;
+
+    @Override
+    public HttpStatus status() {
+        return httpStatus;
+    }
+
+    @Override
+    public String message() {
+        return messageTemplate;
+    }
+}
